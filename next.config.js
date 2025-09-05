84d29cdcf2766e4faceb5b6efed74e1e71ad966b
russellsid <sid.expat@gmail.com>
2025-09-05 13:58:27 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,17 @@
 /** @type {import('next').NextConfig} */
-export default { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Browser build: tell webpack there is no 'fs' / 'path' / 'crypto' in the browser
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...(config.resolve.fallback || {}),
+        fs: false,
+        path: false,
+        crypto: false,
+      };
+    }
+    return config;
+  },
+};
+export default nextConfig;
