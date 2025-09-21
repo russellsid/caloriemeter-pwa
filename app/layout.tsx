bf1d7081f58d046015335b8e548bb8fcd4341747
russellsid <sid.expat@gmail.com>
2025-09-21 17:26:13 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" /> {/* NEW */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
